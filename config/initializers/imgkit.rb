8d8db628cc70d8b443cf4969123b2fad2da01fbe
vavax0 <vavax0@gmail.com>
2014-06-26 22:31:35 +0200

    Added IMGKit for Poduction

--- config/initializers/imgkit.rb
+++ config/initializers/imgkit.rb
@@ -1,7 +1,10 @@
 IMGKit.configure do |config|
 
+
+	config.wkhtmltoimage = Rails.root.join('bin', 'wkhtmltoimage-amd64').to_s if ENV['RACK_ENV'] == 'production'
   config.wkhtmltoimage = 'C:\wkhtmltoimage.exe'
 
   config.default_options = { :quality => 80 }
 
 end
+
